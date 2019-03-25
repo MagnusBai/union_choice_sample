b60f9a0090a43b5f4b231674e31281e9645a4ae9
MagnusBai <mingran@live.com>
2019-03-26 07:23:43 +0800

    final demo

--- test.cpp
+++ test.cpp
@@ -1,20 +1,17 @@
 #include <iostream>
 
+// b选项的数据
 struct X_Range_Setting {
     bool is_auto_xrange;    // 
     float x_start;
     float x_end;
     // https://q.cnblogs.com/q/57184/
-    // union里面不能放有构造函数，析构函数和拷贝构造函数的class的成员变量
+    // union里面的类不能放有构造函数，析构函数和拷贝构造函数的class的成员变量
     // 因为编译器无法决定是否该调用构造函数去初始化成员变量
-    /*
-    X_Range_Setting(float x0, float x1) {
-        // CHECK_GT(x1, x0)
-	x_start = x0;
-	x_end = x1;
-    }*/
 };
 
+// 升级b数据，增加构造函数
+// 直接在X_Range_Setting里放构造函数，会报错
 struct X_Range_Setting_: public X_Range_Setting {
     X_Range_Setting_(float x0, float x1) {
         // CHECK_GT(x1, x0)
@@ -24,39 +21,45 @@ struct X_Range_Setting_: public X_Range_Setting {
     }
 };
 
+// 存储用户选项的结构体
 struct Cfg {
     union X_Status {
-        bool is_auto_x;
-	struct X_Range_Setting x_setting;
+        bool is_auto_x;                     // 选项a：自动决定x范围
+	struct X_Range_Setting x_setting;   // 选项b：手动确定x范围
     } x_status;
-
-    ~Cfg() {
-        if(!x_status.is_auto_x) {
-	    std::cout << "is setting start&end points" << std::endl;
-	    std::cout << "start_point:" << x_status.x_setting.x_start << std::endl;
-	    std::cout << "end_point:" << x_status.x_setting.x_end << std::endl;
-	}
-	else {
-	    std::cout << "is auto range" << std::endl;
+   
+    void process() {
+        switch (x_status.is_auto_x) {
+	    case true: // 处理用户a选项
+	        std::cout << "CHOICE A: auto_range" << std::endl;
+	        break;
+            case false:  // 处理用户b选项
+		std::cout << "CHOICE B: set_range" << std::endl;
+	        std::cout << " start_point: " << x_status.x_setting.x_start;
+	        std::cout << " end_point: " << x_status.x_setting.x_end << std::endl;
+		break;
 	}
     }
 };
 
+// 主测试函数
 void test_cfg() {
-    // test1: is_auto_x
+    // 用户a选项
     Cfg cfg1;
     cfg1.x_status.is_auto_x = true;
+    cfg1.process();
 
-    // test2: has start&end setting
+    // 用户b选项
     Cfg cfg2;
     // cfg2.x_status.x_setting.is_auto_xrange = false;
-    // cfg2.x_status.x_setting.x_start= 1.7;
-    // cfg2.x_status.x_setting.x_end = 7.6;
-    cfg2.x_status.x_setting = X_Range_Setting_(1.783, 0.704);
+    // cfg2.x_status.x_setting.x_start= 1.7f;
+    // cfg2.x_status.x_setting.x_end = 7.6f;
+    cfg2.x_status.x_setting = X_Range_Setting_(1.7f, 7.6f);
+    cfg2.process();
+
 }
 
 int main() {
-    std::cout << "dota2 wtf" << std::endl;
     test_cfg();
     return 0;
 }
